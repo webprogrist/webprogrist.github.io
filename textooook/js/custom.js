116cbe9062136035f494177e43c59b30f1ca59f2
webprogrist <50573992+webprogrist@users.noreply.github.com>
2019-06-08 08:19:55 +0300

    Шо то там

---
+++ textooook/js/custom.js
@@ -0,0 +1,14 @@
+jQuery(function($){
+
+	$(document).ready(function(){
+		$('#sl1,#sl2').slick({
+			  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1300,
+  arrows: false,
+		});
+	});
+
+});
+
