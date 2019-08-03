afe055150703cf4bcd8e7e6cf7e5d2308f2780b5
webprogrist <50573992+webprogrist@users.noreply.github.com>
2019-08-03 16:49:37 +0300

    4

--- Работика)/js/custom.js
+++ Работика)/js/custom.js
@@ -2,11 +2,54 @@ jQuery(function($){
 
 	$(document).ready(function(){
 		$('#sl1,#sl2').slick({
-			  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1300,
-  arrows: false,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 1300,
+			arrows: false,
+			adaptiveHeight: true,
+			mobileFirst:true,
+			responsive: [
+				{
+					breakpoint: 961,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 1,
+						autoplay: true,
+						autoplaySpeed: 1300,
+						arrows: false,
+
+					}
+
+				},
+				{
+					breakpoint: 482,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 1,
+						autoplay: true,
+						autoplaySpeed: 1300,
+						arrows: false,
+
+					}
+
+				},
+				{
+					breakpoint: 320,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						autoplay: true,
+						autoplaySpeed: 1300,
+						arrows: false,
+
+					}
+
+				}
+
+			]
+
+
 		});
 	});
 
