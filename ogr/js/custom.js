160d8d6e3d6cc32295c264544075ef35ae3d4132
webprogrist <50573992+webprogrist@users.noreply.github.com>
2019-08-07 18:01:27 +0300

    zepa

---
+++ ogr/js/custom.js
@@ -0,0 +1,57 @@
+jQuery(function($){
+
+	$(document).ready(function(){
+		$('#sl1,#sl2').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 1300,
+			arrows: false,
+			adaptiveHeight: true,
+			mobileFirst:true,
+			responsive: [
+				{
+					breakpoint: 961,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 1,
+						autoplay: true,
+						autoplaySpeed: 1300,
+						arrows: false,
+
+					}
+
+				},
+				{
+					breakpoint: 482,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 1,
+						autoplay: true,
+						autoplaySpeed: 1300,
+						arrows: false,
+
+					}
+
+				},
+				{
+					breakpoint: 319,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						autoplay: true,
+						autoplaySpeed: 1300,
+						arrows: false,
+
+					}
+
+				}
+
+			]
+
+
+		});
+	});
+
+});
+
